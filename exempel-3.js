d31d1375554e2d169079f86c2e274fdc24743319
Fobiwa <folke@wibom.nu>
2018-10-03 13:53:59 +0200

    js commit

---
+++ exempel-3.js
@@ -0,0 +1,21 @@
+//Arbeta med primitiva typer (datatyper)
+
+//1. String
+let firstName = "Folke";
+let lastName = "Wibom";
+console.log(firstName, lastName);
+console.log("It's very nice!");
+
+
+//2. Number
+let price = 19;
+let quantity = 10;
+let total = price * quantity;
+console.log(total);
+
+//3. Boolean
+let ok = false;
+let again = true;
+if (again)
+    console.log("Fortsätt...");
+
